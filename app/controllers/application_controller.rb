3a4535f07be63e8dd8367c4db226ccffa4151de6
Shutto <baw.keepon69@gmail.com>
2019-07-24 01:25:51 +0900

    done 4-5(login requirement)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :login_requierd
+
+  private
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_requierd
+    redirect_to login_url unless current_user
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :require_admin
+
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -15,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
@@ -25,17 +27,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
     @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{@task.name}を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
@@ -43,5 +45,9 @@ class TasksController < ApplicationController
   private
     def task_params
       params.require(:task).permit(:name, :description)
+    end
+
+    def require_admin
+      redirect_to root_url unless current_user.admin?
     end
 end
