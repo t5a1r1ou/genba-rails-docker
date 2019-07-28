81551d776e31b611fd59a50469671698beef1b0d
Shutto <baw.keepon69@gmail.com>
2019-07-28 16:27:20 +0900

    done chapter6

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+    config.time_zone = 'Asia/Tokyo'
+
     config.generators do |g|
       g.test_framework = "rspec"
       g.helper_specs = false
