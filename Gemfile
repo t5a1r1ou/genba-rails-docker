cbd2402d86345a1b6438826cbec6a35871cbc5b1
Shutto <baw.keepon69@gmail.com>
2019-07-30 00:00:52 +0900

    done 7-4

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'rails_autolink'
 
+gem 'ransack'
+
+gem 'mailcatcher'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   def show
@@ -18,11 +19,22 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
 
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
+      TaskMailer.creation_email(@task).deliver_now
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   def edit
